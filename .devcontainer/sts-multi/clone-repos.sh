1d00dcbe2a866b631619bf9906dc475be4de0282
Kevin <belay.00.theorem@icloud.com>
2024-10-28 15:17:30 +0000

    upd docs and scripts

--- .devcontainer/sts-multi/clone-repos.sh
+++ .devcontainer/sts-multi/clone-repos.sh
@@ -5,8 +5,13 @@ clone_repo() {
   gh repo clone SubaruTechShare/$repo_name /workspaces/$repo_name
 }
 
-
-
 clone_repo sts-client
 clone_repo sts-backend
 clone_repo techshare-backend
+
+$(cd /workspaces/sts-backend && aqua install)
+
+# aws cli and aws-sso-cli prerequisites
+
+mkdir $HOME/.aws-sso $HOME/.aws
+touch $HOME/.aws-sso/store.json
---
+++ CODESPACES.md
@@ -0,0 +1,7 @@
+# Codespaces usage
+
+If you are opening this repo as a codespaces user, [a script has run](./.devcontainer/sts-multi/clone-repos.sh) which cloned STS repos to `/workspaces`. You can run in a terminal `code /workspaces/sts-backend` (or other repo) to open the repo you're expecting in visual studio code.
+
+# IntelliJ
+
+TBD
